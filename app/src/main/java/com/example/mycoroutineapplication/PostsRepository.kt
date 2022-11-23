1cb86ad2ebd277014b1d6d105a9dd14e15fdf7d4
syi08077@gmail.com <syi08077@gmail.com>
2022-11-23 16:28:30 +0900

    13week

---
+++ app/src/main/java/com/example/mycoroutineapplication/PostsRepository.kt
@@ -0,0 +1,27 @@
+package com.example.mycoroutineapplication
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import java.net.HttpURLConnection
+import java.net.URL
+
+class PostsRepository {
+    private val topURLStr = "https://jsonplaceholder.typicode.com/"
+
+    suspend fun readPosts() : JSONArray? {
+        val reqUrl = URL(topURLStr + "posts")
+        return withContext(Dispatchers.IO) {
+            (reqUrl.openConnection() as HttpURLConnection)?.run {
+                requestMethod = "GET"
+                connect()
+                val reader = inputStream.bufferedReader()
+                val strBuilder = StringBuilder()
+                while (true) {
+                    strBuilder.append(reader.readLine() ?: break)
+                }
+                JSONArray(strBuilder.toString())
+            }
+        }
+    }
+}
